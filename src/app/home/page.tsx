aa9fcea99482c79eb27d5e34ddcbdb5a8735c839
vosonha89@gmail.com <vosonha89>
2025-04-11 16:55:16 +0700

    update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,14 +7,11 @@ import { HomeServerState } from './server/Home.state';
 import Home from './client/Home';
 
 export default async function Page({
-  params,
   searchParams,
 }: Readonly<{
   params: Promise<{ slug: string }>,
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }>) {
-  const { slug } = await params;
-  console.log(slug);
   const languageService = container.resolve(ServerLanguageService);
   languageService.getLanguageInParam(searchParams);
 
